7a9f245f34867a3dff30bdae81ccb01132411279
Coding With Shawn <shawnray5699@gmail.com>
2023-10-19 16:09:44 -0500

    Update WayPointFollower.cs
    commented WayPointFollower.cs

--- game/Assets/Scripts/WayPointFollower.cs
+++ game/Assets/Scripts/WayPointFollower.cs
@@ -1,24 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System.Collections; // This line is importing the System.Collections namespace which provides interfaces and classes that define various collections of objects, such as lists, queues, bit arrays, hash tables and dictionaries.
 
-public class WayPointFollower : MonoBehaviour
+using System.Collections.Generic; // This line is importing the System.Collections.Generic namespace which provides generic collection classes. The term 'generic' means the same class can be used with many different types of data.
+
+using UnityEngine; // This line is importing the UnityEngine namespace which contains all the classes you need to create scripts for your Unity game.
+
+public class WayPointFollower : MonoBehaviour // This line is declaring a public class named 'WayPointFollower' that inherits from the 'MonoBehaviour' class. MonoBehaviour is the base class from which every Unity script derives.
+
 {
-    [SerializeField] private GameObject[] waypoints;
-    public int currentWaypointIndex = 0;
-    [SerializeField] private float speed = 2f;
+    [SerializeField] private GameObject[] waypoints; // This line is declaring a serialized private array of GameObjects named 'waypoints'. The [SerializeField] attribute allows private fields to be visible in the Unity editor.
 
-    private void Update()
+    public int currentWaypointIndex = 0; // This line is declaring a public integer named 'currentWaypointIndex' and initializing it to 0. This variable will keep track of which waypoint the object is currently moving towards.
+
+    [SerializeField] private float speed = 2f; // This line is declaring a serialized private float named 'speed' and initializing it to 2. The [SerializeField] attribute allows private fields to be visible in the Unity editor.
+
+    private void Update() // This method is called once per frame in Unity.
+
     {
-        if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .1f)
+        if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .1f) // This line checks if the distance between the current waypoint and this object's position is less than 0.1 units.
+
         {
-            currentWaypointIndex++;
-            if (currentWaypointIndex >= waypoints.Length)
+            currentWaypointIndex++; // If it is, this line increments 'currentWaypointIndex' by one, so the object will start moving towards the next waypoint.
+
+            if (currentWaypointIndex >= waypoints.Length) // This line checks if 'currentWaypointIndex' is greater than or equal to the length of the 'waypoints' array.
+
             { 
-            currentWaypointIndex = 0;
+            currentWaypointIndex = 0; // If it is, this line resets 'currentWaypointIndex' back to 0, so the object will start moving towards the first waypoint again.
             }
         }
-        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, speed * Time.deltaTime);
-
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, speed * Time.deltaTime); // This line moves this object towards the current waypoint at a speed adjusted for frame rate.
     }
 }
+
+//This script appears to be for an object in a game that moves along a set path defined by waypoints.
+
