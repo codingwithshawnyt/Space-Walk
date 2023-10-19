b10e44508805289f76ae35599ef06b69617f242d
Coding With Shawn <shawnray5699@gmail.com>
2023-10-19 16:06:19 -0500

    Update StartMenu.cs
    comment StartMenu.cs

--- game/Assets/Scripts/StartMenu.cs
+++ game/Assets/Scripts/StartMenu.cs
@@ -1,12 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+using System.Collections; // This line is importing the System.Collections namespace which provides interfaces and classes that define various collections of objects, such as lists, queues, bit arrays, hash tables and dictionaries.
 
-public class StartMenu : MonoBehaviour
+using System.Collections.Generic; // This line is importing the System.Collections.Generic namespace which provides generic collection classes. The term 'generic' means the same class can be used with many different types of data.
+
+using UnityEngine; // This line is importing the UnityEngine namespace which contains all the classes you need to create scripts for your Unity game.
+
+using UnityEngine.SceneManagement; // This line is importing the UnityEngine.SceneManagement namespace which provides classes to manage your game scenes.
+
+public class StartMenu : MonoBehaviour // This line is declaring a public class named 'StartMenu' that inherits from the 'MonoBehaviour' class. MonoBehaviour is the base class from which every Unity script derives.
+
 {
-    public void StartGame()
+    public void StartGame() // This line is declaring a public method named 'StartGame'. This method can be called from other scripts or in response to a user action like a button click.
+
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // This line is calling the 'LoadScene' method on the 'SceneManager' class. It loads a new scene based on the build index of the currently active scene plus one. In other words, it loads the next scene in your game.
     }
 }
+
+//This script appears to be part of a start menu for a game. When the StartGame method is called, it advances the game to the next scene.
+
