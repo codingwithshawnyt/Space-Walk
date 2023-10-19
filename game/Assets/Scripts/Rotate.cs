d51133092d13057e557d8701adc2f61a9c627552
Coding With Shawn <shawnray5699@gmail.com>
2023-10-19 16:04:31 -0500

    Update Rotate.cs
    comment rotate.cs

--- game/Assets/Scripts/Rotate.cs
+++ game/Assets/Scripts/Rotate.cs
@@ -1,15 +1,26 @@
+// Importing the System.Collections namespace which provides interfaces and classes that define various collections of objects, such as lists, queues, bit arrays, hash tables and dictionaries.
 using System.Collections;
+
+// Importing the System.Collections.Generic namespace which contains interfaces and classes that define generic collections, which allow users to create strongly typed collections that provide better type safety and performance than non-generic strongly typed collections.
 using System.Collections.Generic;
+
+// Importing the UnityEngine namespace which contains all of the classes, structures and enumerations that Unity uses.
 using UnityEngine;
 
+// Declaring a public class named Rotate that inherits from MonoBehaviour. MonoBehaviour is the base class from which every Unity script derives.
 public class Rotate : MonoBehaviour
 {
+    // Declaring a private float variable named speed. The [SerializeField] attribute allows private fields to be visible in the Unity editor.
     [SerializeField] private float speed = 2f;
 
-
-    // Update is called once per frame
+    // Update is called once per frame. It's used for regular updates such as moving non-physics objects.
     void Update()
     {
+        // Rotating the object this script is attached to around the z-axis at a rate of 360 degrees times speed per second. 
+        // Time.deltaTime is used to make the rotation smooth and frame rate independent.
         transform.Rotate(0, 0, 360 * speed * Time.deltaTime);
     }
 }
+
+//This script is typically used in a game where you want an object to rotate continuously. The Update method is called once per frame and rotates the object this script is attached to around the z-axis at a rate of 360 * speed degrees per second. Time.deltaTime is used to make the rotation smooth and frame rate independent. The speed variable can be set in the Unity editor thanks to the [SerializeField] attribute. If you attach this script to an object in your Unity game, that object will rotate around its z-axis continuously. user Comment each line of this: using System.Collections; using System.Collections.Generic; using UnityEngine;
+
