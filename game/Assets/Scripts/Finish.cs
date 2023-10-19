8fa0eda07b904bbd88dcfb590a5bee189e23c540
Coding With Shawn <shawnray5699@gmail.com>
2023-10-19 15:50:07 -0500

    Update Finish.cs
    commenting Finish.cs

--- game/Assets/Scripts/Finish.cs
+++ game/Assets/Scripts/Finish.cs
@@ -1,22 +1,33 @@
+// Importing the System.Collections namespace which provides interfaces and classes that define various collections of objects, such as lists, queues, bit arrays, hash tables and dictionaries.
 using System.Collections;
+
+// Importing the System.Collections.Generic namespace which contains interfaces and classes that define generic collections, which allow users to create strongly typed collections that provide better type safety and performance than non-generic strongly typed collections.
 using System.Collections.Generic;
+
+// Importing the UnityEngine namespace which contains all of the classes, structures and enumerations that Unity uses.
 using UnityEngine;
 
+// Declaring a public class named Finish that inherits from MonoBehaviour. MonoBehaviour is the base class from which every Unity script derives.
 public class Finish : MonoBehaviour
 {
+    // Declaring a private AudioSource variable named finishSound. This will be used to play a sound when the player finishes.
     private AudioSource finishSound;
-    // Start is called before the first frame update
+
+    // The Start method is called before the first frame update. It's used for initialization.
     void Start()
     {
+        // Getting the AudioSource component attached to this object and assigning it to the finishSound variable.
         finishSound = GetComponent<AudioSource>();
     }
 
-
+    // OnTriggerEnter2D is called when another object enters a trigger collider attached to this object (2D physics only).
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Checking if the object that collided with the trigger has a name of "Player".
         if (collision.gameObject.name == "Player")
+            // If it does, play the finish sound.
             finishSound.Play();
     }
-
-
 }
+
+//This script is typically used in a 2D game where you want a sound to play when the player reaches a certain point (like a finish line). The OnTriggerEnter2D method is called when another 2D collider enters this object’s trigger collider. If that object is named “Player”, it plays the finishSound. The GetComponent<AudioSource>() call in the Start method gets the AudioSource component attached to this object (which should be set up to play your desired sound) and assigns it to finishSound. This script should be attached to your finish line object in your Unity game. The “Player” object should have a 2D collider on it set as a trigger. When the player enters the finish line’s trigger collider, it will play the sound. Note that this script assumes you have set up an AudioSource on your finish line object with your desired sound. If not, GetComponent<AudioSource>() will return null and trying to call Play() on it will result in a NullReferenceException. Make sure you have set up an AudioSource on your finish line object in Unity.
