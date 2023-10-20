26f784092120d4f82f1d6517244019886a108053
Coding With Shawn <codingwithshawn@hash.fyi>
2023-10-20 12:27:55 -0500

    Updating comments on PlayerLife.cs for accuracy and clarity

--- game/Assets/Scripts/PlayerLife.cs
+++ game/Assets/Scripts/PlayerLife.cs
@@ -33,7 +33,7 @@ public class PlayerLife : MonoBehaviour
         // Checking if the object that collided with this one has a tag of "trap".
         if (collision.gameObject.CompareTag("trap"))
         { 
-            // If it does, call the Death method.
+            // If it does, call the Death method (below).
             Death();
         }
        
@@ -57,5 +57,12 @@ public class PlayerLife : MonoBehaviour
     }
 }
 
-//This script is typically used in a game where you want something to happen when your player character dies (like playing a death animation and restarting the level). The Start method gets the Rigidbody2D and Animator components attached to this object (which should be your player character) and assigns them to rb and anim. The OnCollisionEnter2D method is called when this object’s collider begins touching another collider. If that other collider has a tag of “trap”, it calls Death. The Death method sets rb.bodyType to RigidbodyType2D.Static, which makes it unaffected by forces or collisions, and sets the “death” trigger on anim, which should play a death animation. The RestartLevel method loads the scene with the same name as the currently active scene, effectively restarting the level. Note that this script assumes you have set up an animation for your player character’s death in Unity with a trigger named “death”. If not, anim.SetTrigger("death"); will not work. Also note that this script assumes you have added a tag of “trap” to any objects in your scene that should kill your player character on contact. If not, collision.gameObject.CompareTag("trap") will always be false. Make sure you have set up these things in Unity for this script to work as expected.
+/* This script is typically used in a game where you want something to happen when your player character dies (like playing a death animation and restarting the level). 
+The Start method gets the Rigidbody2D and Animator components attached to this object (which should be your player character) and assigns them to rb and anim. 
+The OnCollisionEnter2D method is called when this object’s collider begins touching another collider. If that other collider has a tag of “trap”, it calls Death. 
+The Death method sets rb.bodyType to RigidbodyType2D.Static, which makes it unaffected by forces or collisions, and sets the “death” trigger on anim, which should play a death animation. 
+The RestartLevel method loads the scene with the same name as the currently active scene, effectively restarting the level. 
+Note that this script assumes you have set up an animation for your player character’s death in Unity with a trigger named “death”. 
+If not, anim.SetTrigger("death"); will not work. Also note that this script assumes you have added a tag of “trap” to any objects in your scene that should kill your player character on contact. 
+If not, collision.gameObject.CompareTag("trap") will always be false. Make sure you have set up these things in Unity for this script to work as expected. */
 
