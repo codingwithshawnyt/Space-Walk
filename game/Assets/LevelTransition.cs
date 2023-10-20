bfc90f94ba2fb289c5f7dfdabe35c13d5f7501db
Coding With Shawn <codingwithshawn@hash.fyi>
2023-10-20 12:37:34 -0500

    Adding namespace comments for LevelTransition.cs

--- game/Assets/LevelTransition.cs
+++ game/Assets/LevelTransition.cs
@@ -1,7 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+using System.Collections; // This line is importing the System.Collections namespace which provides interfaces and classes that define various collections of objects, such as lists, queues, bit arrays, hash tables and dictionaries.
+using System.Collections.Generic; // This line is importing the System.Collections.Generic namespace which provides generic collection classes. The term 'generic' means the same class can be used with many different types of data.
+using UnityEngine; // This line is importing the UnityEngine namespace which contains all the classes you need to create scripts for your Unity game.
+using UnityEngine.SceneManagement; // This line is importing the UnityEngine.SceneManagement namespace which provides classes to manage your game scenes.
 
 
 public class LevelTransition : MonoBehaviour
