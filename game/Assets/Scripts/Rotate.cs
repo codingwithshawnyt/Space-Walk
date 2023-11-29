8edbee9a24d0beaa21842802e6c37daa9cfce292
Coding With Shawn <codingwithshawn@hash.fyi>
2023-11-29 12:20:49 -0600

    comment concision (one at a time)

--- game/Assets/Scripts/Rotate.cs
+++ game/Assets/Scripts/Rotate.cs
@@ -1,23 +1,23 @@
-// Importing the System.Collections namespace which provides interfaces and classes that define various collections of objects, such as lists, queues, bit arrays, hash tables and dictionaries.
+//Importing the System.Collections namespace which provides interfaces and classes that define various collections of objects, such as lists, queues, bit arrays, hash tables and dictionaries.
 using System.Collections;
 
-// Importing the System.Collections.Generic namespace which contains interfaces and classes that define generic collections which allow for strongly typed collections that provide better type safety and performance than non-generic strongly typed collections.
+//Importing the System.Collections.Generic namespace which contains interfaces and classes that define generic collections which allow for strongly typed collections that provide better type safety and performance than non-generic strongly typed collections.
 using System.Collections.Generic;
 
-// Importing the UnityEngine namespace which contains all of the classes, structures and enumerations that Unity uses.
+//Importing the UnityEngine namespace which contains all of the classes, structures and enumerations that Unity uses.
 using UnityEngine;
 
-// Declaring a public class named Rotate that inherits from MonoBehaviour. MonoBehaviour is the base class from which every Unity script derives.
+//Declaring a public class named Rotate that inherits from MonoBehaviour. MonoBehaviour is the base class from which every Unity script derives.
 public class Rotate : MonoBehaviour
 {
-    // Declaring a private float variable named speed. The [SerializeField] attribute allows private fields to be visible in the Unity editor.
+    //Declaring a private float variable named speed. The [SerializeField] attribute allows private fields to be visible in the Unity editor.
     [SerializeField] private float speed = 2f;
 
-    // Update is called once per frame. It's used for regular updates such as moving non-physics objects.
+    //Update is called once per frame. It's used for regular updates such as moving non-physics objects.
     void Update()
     {
-        // Rotating the object this script is attached to around the z-axis at a rate of 360 degrees times speed per second. 
-        // Time.deltaTime is used to make the rotation smooth and frame rate independent.
+        //Rotating the object this script is attached to around the z-axis at a rate of 360 degrees times speed per second. 
+        //Time.deltaTime is used to make the rotation smooth and frame rate independent.
         transform.Rotate(0, 0, 360 * speed * Time.deltaTime);
     }
 }
