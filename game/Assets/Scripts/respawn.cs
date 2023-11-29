fb6ff0eb26a3b4361c13b228a2218a34f3bcdf51
Coding With Shawn <codingwithshawn@hash.fyi>
2023-11-29 12:20:32 -0600

    comment concision (one at a time)

--- game/Assets/Scripts/respawn.cs
+++ game/Assets/Scripts/respawn.cs
@@ -1,24 +1,24 @@
-using System.Collections; // This line is importing the System.Collections namespace which provides interfaces and classes that define various collections of objects, such as lists, queues, bit arrays, hash tables and dictionaries.
+using System.Collections; //This line is importing the System.Collections namespace which provides interfaces and classes that define various collections of objects, such as lists, queues, bit arrays, hash tables and dictionaries.
 
-using System.Collections.Generic; // This line is importing the System.Collections.Generic namespace which provides generic collection classes. The term 'generic' means the same class can be used with many different types of data.
+using System.Collections.Generic; //This line is importing the System.Collections.Generic namespace which provides generic collection classes. The term 'generic' means the same class can be used with many different types of data.
 
-using UnityEngine; // This line is importing the UnityEngine namespace which contains all the classes you need to create scripts for your Unity game.
+using UnityEngine; //This line is importing the UnityEngine namespace which contains all the classes you need to create scripts for your Unity game.
 
-using UnityEngine.SceneManagement; // This line is importing the UnityEngine.SceneManagement namespace which provides classes to manage your game scenes.
+using UnityEngine.SceneManagement; //This line is importing the UnityEngine.SceneManagement namespace which provides classes to manage your game scenes.
 
-public class respawn : MonoBehaviour // This line is declaring a public class named 'respawn' that inherits from the 'MonoBehaviour' class. MonoBehaviour is the base class from which every Unity script derives.
+public class respawn : MonoBehaviour //This line is declaring a public class named 'respawn' that inherits from the 'MonoBehaviour' class. MonoBehaviour is the base class from which every Unity script derives.
 
 {
     
-     public float threshold; // This line is declaring a public float variable named 'threshold'. This variable will be used to determine when the object should respawn.
+     public float threshold; //This line is declaring a public float variable named 'threshold'. This variable will be used to determine when the object should respawn.
 
-    void FixedUpdate() // This method is called every fixed framerate frame in Unity. It's commonly used to update physics-related information.
+    void FixedUpdate() //This method is called every fixed framerate frame in Unity. It's commonly used to update physics-related information.
 
     {
-        if (transform.position.y < threshold) // This line checks if the y-coordinate of this object's position is less than 'threshold'.
+        if (transform.position.y < threshold) //This line checks if the y-coordinate of this object's position is less than 'threshold'.
 
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name); // If it is, this line reloads the current scene. This effectively "respawns" all objects in the scene to their initial positions.
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name); //If it is, this line reloads the current scene. This effectively "respawns" all objects in the scene to their initial positions.
         }
 
 
