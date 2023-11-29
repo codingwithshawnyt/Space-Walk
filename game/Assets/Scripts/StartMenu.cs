d30a1c9e4f7499b9bdcb551eb69ca115a9147dce
Coding With Shawn <codingwithshawn@hash.fyi>
2023-11-29 12:21:13 -0600

    comment concision (one at a time)

--- game/Assets/Scripts/StartMenu.cs
+++ game/Assets/Scripts/StartMenu.cs
@@ -1,18 +1,18 @@
-using System.Collections; // This line is importing the System.Collections namespace which provides interfaces and classes that define various collections of objects, such as lists, queues, bit arrays, hash tables and dictionaries.
+using System.Collections; //This line is importing the System.Collections namespace which provides interfaces and classes that define various collections of objects, such as lists, queues, bit arrays, hash tables and dictionaries.
 
-using System.Collections.Generic; // This line is importing the System.Collections.Generic namespace which provides generic collection classes. The term 'generic' means the same class can be used with many different types of data.
+using System.Collections.Generic; //This line is importing the System.Collections.Generic namespace which provides generic collection classes. The term 'generic' means the same class can be used with many different types of data.
 
-using UnityEngine; // This line is importing the UnityEngine namespace which contains all the classes you need to create scripts for your Unity game.
+using UnityEngine; //This line is importing the UnityEngine namespace which contains all the classes you need to create scripts for your Unity game.
 
-using UnityEngine.SceneManagement; // This line is importing the UnityEngine.SceneManagement namespace which provides classes to manage your game scenes.
+using UnityEngine.SceneManagement; //This line is importing the UnityEngine.SceneManagement namespace which provides classes to manage your game scenes.
 
-public class StartMenu : MonoBehaviour // This line is declaring a public class named 'StartMenu' that inherits from the 'MonoBehaviour' class. MonoBehaviour is the base class from which every Unity script derives.
+public class StartMenu : MonoBehaviour //This line is declaring a public class named 'StartMenu' that inherits from the 'MonoBehaviour' class. MonoBehaviour is the base class from which every Unity script derives.
 
 {
-    public void StartGame() // This line is declaring a public method named 'StartGame'. This method can be called from other scripts or in response to a user action like a button click.
+    public void StartGame() //This line is declaring a public method named 'StartGame'. This method can be called from other scripts or in response to a user action like a button click.
 
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // This line is calling the 'LoadScene' method on the 'SceneManager' class. It loads a new scene based on the build index of the currently active scene plus one. In other words, it loads the next scene in your game.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //This line is calling the 'LoadScene' method on the 'SceneManager' class. It loads a new scene based on the build index of the currently active scene plus one. In other words, it loads the next scene in your game.
     }
 }
 
